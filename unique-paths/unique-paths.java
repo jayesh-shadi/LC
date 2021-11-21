e2565ed89e1a380d146dd1aa4a7b13ef589987cd
Jayesh Shadi <44752916+jayesh-shadi@users.noreply.github.com>
2021-11-21 17:32:26 +0530

    Time: 0 ms (100.00%), Space: 36 MB (41.88%) - LeetHub

--- unique-paths/unique-paths.java
+++ unique-paths/unique-paths.java
@@ -1,20 +1,19 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] t=new int[n][m];
-        for(int i=0;i<n;i++)
+        int[][] t=new int[m][n];
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<n;j++)
             {
-                if(i==0 || j==0) 
+                if(i==0||j==0)
                 {
                     t[i][j]=1;
-                    
                 }
                 else{
                     t[i][j]=t[i-1][j]+t[i][j-1];
                 }
             }
         }
-                      return t[n-1][m-1];
+        return t[m-1][n-1];
     }
 }
