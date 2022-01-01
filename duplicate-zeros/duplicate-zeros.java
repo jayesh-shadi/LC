86072f522638ce5639e5651f4c332184d6af0dc6
Jayesh Shadi <44752916+jayesh-shadi@users.noreply.github.com>
2022-01-01 21:28:33 +0530

    Time: 33 ms (5.15%), Space: 42.9 MB (5.83%) - LeetHub

---
+++ duplicate-zeros/duplicate-zeros.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        for(int i=0;i<arr.length;i++)
+        {
+           
+            if(arr[i]==0)
+            {
+                 int j;
+                for(j=arr.length-2;j>=i+1;j--)
+                {
+                    arr[j+1]=arr[j];
+                }
+                   arr[j+1]=0;
+                      i++;
+            }
+         
+        }
+    }
+}
