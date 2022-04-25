2984f3b608809a1596d0b674f033f52c7e8ebb83
Jayesh Shadi <44752916+jayesh-shadi@users.noreply.github.com>
2022-04-25 20:07:29 +0530

    Time: 2 ms (53.31%), Space: 45.2 MB (29.13%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -1,25 +1,43 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        if(nums==null || nums.length==0)return 0;
-        int start=0,end=0,sum=0,minlength=Integer.MAX_VALUE;
-        while(end<nums.length) // 1 . 0< 6
-            // 1<6 
-            //2<6
-            //3<6 
-            //4<6 
-        {
-            sum+=nums[end++]; // 0 +2 
-             // iteration 2    2 + 3=5
-            // iteration 3    5+1=6 
-            //iteration 4    6+2 
+        
+        //BRUTEFORCE SOLUTION (2 POINTER METHOD)
+//         if(nums==null || nums.length==0)return 0;
+//         int start=0,end=0,sum=0,minlength=Integer.MAX_VALUE;
+//         while(end<nums.length) // 1 . 0< 6
+//             // 1<6 
+//             //2<6
+//             //3<6 
+//             //4<6 
+//         {
+//             sum+=nums[end++]; // 0 +2 
+//              // iteration 2    2 + 3=5
+//             // iteration 3    5+1=6 
+//             //iteration 4    6+2 
             
-            while(sum>=target) 
-            {
-                minlength=Math.min(minlength,end-start);
-                sum-=nums[start++];
-            }
+//             while(sum>=target) 
+//             {
+//                 minlength=Math.min(minlength,end-start);
+//                 sum-=nums[start++];
+//             }
+//         }
+//          return minlength == Integer.MAX_VALUE ? 0 : minlength; int i=0;
+    int j=0;
+        int i=0;
+    int min=Integer.MAX_VALUE; 
+    int sum=0;
+    while(i<=j && j<nums.length){
+        if(sum+nums[j]>=target){  //if current sum>=target then update min length
+            min=Math.min(min,j-i+1);
+            sum-=nums[i];   //decrement from left i.e decrease window size
+            i++;
         }
-         return minlength == Integer.MAX_VALUE ? 0 : minlength;
+        else{
+            sum+=nums[j];
+            j++;  //if current sum<target then increase window size
+        }
+    }
+    return min==Integer.MAX_VALUE?0:min;
         
     }
     
