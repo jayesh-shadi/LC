99971394115a72991dbb394fea6d74ef7cceb332
Jayesh Shadi <44752916+jayesh-shadi@users.noreply.github.com>
2022-07-03 00:20:51 +0530

    Time: 10 ms (24.93%), Space: 45 MB (17.48%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.java
+++ 414-third-maximum-number/414-third-maximum-number.java
@@ -1,21 +1,23 @@
 class Solution {
-    public int thirdMax(int[] nums) {
-        Integer max1 = null;
-        Integer max2 = null;
-        Integer max3 = null;
-        for (Integer n : nums) {
-            if (n.equals(max1) || n.equals(max2) || n.equals(max3)) continue;
-            if (max1 == null || n > max1) {
-                max3 = max2;
-                max2 = max1;
-                max1 = n;
-            } else if (max2 == null || n > max2) {
-                max3 = max2;
-                max2 = n;
-            } else if (max3 == null || n > max3) {
-                max3 = n;
-            }
+     public int thirdMax(int[] nums) {
+        //init  treeSet
+        //populate set
+        //if the set size >= 3 pop off first 2 maxes
+        //then return last element
+        
+        TreeSet<Integer> set = new TreeSet<>();
+    
+        for(int i : nums)
+        {
+            set.add(i);
         }
-        return max3 == null ? max1 : max3;
+        
+        if(set.size() >= 3)
+        {
+            set.pollLast();
+            set.pollLast();
+        }
+        
+        return set.last();
     }
 }
