d7035ece58d746c3264ab5c8205f801d3da95a26
Jayesh Shadi <44752916+jayesh-shadi@users.noreply.github.com>
2022-08-09 23:43:34 +0530

    Time: 2 ms (74.13%), Space: 75.5 MB (23.23%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,6 +1,7 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int n=nums.length;
+        //kadanes algorithm o(n) solution
+        int n=nums.length; 
         int lmax=0;
         int gmax=Integer.MIN_VALUE;
         for(int i=0;i<n;i++)
