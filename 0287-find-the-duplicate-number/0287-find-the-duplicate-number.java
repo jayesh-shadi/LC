d2aecbfb06cbcd748cd34ed97bbf3ed47204041c
Jayesh Shadi <44752916+jayesh-shadi@users.noreply.github.com>
2025-04-15 00:02:34 +0530

    Time: 21 ms (29.95%), Space: 57 MB (94.33%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,24 +1,14 @@
 class Solution {
-    public int findDuplicate(int[] nums) { // o(n) time and o(1) space solution by striver
-       int slow = 0;
-        int fast = 0;
-        do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
-
-        slow = 0;
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num:nums)
+        {
+            if(set.contains(num))
+            {
+                return num;
+            }
+            set.add(num);
         }
-        
-        return slow;
-        
-    
-        
-
-            
-        
+        return -1;
     }
 }
