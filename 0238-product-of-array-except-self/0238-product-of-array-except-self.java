e26ffc1b1c10f1a8e5fb1ca252701984405323b0
Jayesh Shadi <44752916+jayesh-shadi@users.noreply.github.com>
2025-04-14 23:46:29 +0530

    Time: 2 ms (88.04%), Space: 56.4 MB (11.5%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -13,6 +13,10 @@ class Solution {
             leftProduct[i]=leftProduct[i-1]*nums[i-1];
         }
         //fill right product
+        //i	rightProduct[i+1]	nums[i+1]	rightProduct[i]
+        // 2	1	4	1 * 4 = 4
+        // 1	4	3	4 * 3 = 12
+        // 0	12	2	12 * 2 = 24
         rightProduct[n-1]=1;
         for(int i=n-2;i>=0;i--)
         {
