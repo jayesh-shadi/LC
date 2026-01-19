932c5ba6658852b8e350ea21883d4c80e4b9da6a
Jayesh Shadi <44752916+jayesh-shadi@users.noreply.github.com>
2026-01-19 23:18:31 +0530

    Time: 5 ms (72.88%), Space: 45.9 MB (78.11%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,18 +1,36 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = String.valueOf(x);
-        int k=0;
-        int j=s.length()-1;
+        //String Approach is not optimal as it consumes extra space of string o(n)
+        // String s = String.valueOf(x);
+        // int k=0;
+        // int j=s.length()-1;
 
-        for(int i=0;i<s.length()-1;i++)
+        // for(int i=0;i<s.length()-1;i++)
+        // {
+        //     if(s.charAt(k)!=s.charAt(j))
+        //     {
+        //         return false;
+        //     }
+        //     k++;
+        //     j--;
+        // }
+        // return true;
+
+        if(x<0 || (x%10==0 && x>0))
         {
-            if(s.charAt(k)!=s.charAt(j))
-            {
-                return false;
-            }
-            k++;
-            j--;
+            return false;
         }
-        return true;
+          int reversed = 0;
+
+        // Reverse only half of the number
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+
+        // For even digits: x == reversed
+        // For odd digits: x == reversed / 10
+        return x == reversed || x == reversed / 10;
+
     }
 }
